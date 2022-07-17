5ff0a8bdf4731bfde59a6ef845ecb3879eeb6249
Deep Vasan <deepvasan10@gmail.com>
2022-07-18 01:31:04 +0530

    Initial commit

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import {SpeechProvider} from "@speechly/react-client";
 import "./index.css";
 
 ReactDOM.render(
-    <SpeechProvider appId="3e1faf3c-20a9-4009-857d-2fc98c9c6c6c" language="en-US">
+    <SpeechProvider appId={process.env.REACT_APP_APPID} language="en-US">
         <Provider>
         <App />
     </Provider>
